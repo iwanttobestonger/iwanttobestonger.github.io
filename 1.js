f58ddf22cb013762495d2f61a8fc7b009dc6c116
crazy_yang <1817414403@qq.com>
2024-09-11 17:53:14 +0800

    01

--- 1.js
+++ 1.js
@@ -1,4 +1,5 @@
 alert("你好，xxxx，网站正在建设中！");
 const button = document.createElement('button');
-button.textContent = '下一页';
+button.textContent = '切换用户';
+button.className = 'move-up'; // 添加类名
 document.body.appendChild(button);
